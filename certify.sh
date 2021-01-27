9c53d70ead960ad028664e5a69b6e042d3b8699e
Naveen Ailawadi <naveen.ailawadi91@gmail.com>
2021-01-27 16:34:02 +0000

    moved certbot to setup

--- certify.sh
+++ certify.sh
@@ -1,10 +1,3 @@
-sudo apt-get update
-sudo apt-get install software-properties-common
-sudo add-apt-repository universe
-sudo add-apt-repository ppa:certbot/certbot
-sudo apt-get update
-
-sudo apt-get install certbot python3-certbot-apache
 sudo certbot --apache
 
 # resource: https://certbot.eff.org/lets-encrypt/ubuntuxenial-apache
